8949c9a6d001cef44528d0186bf24428ad85550a
Kyle Benson <kylewaynebenson@gmail.com>
2017-06-26 15:53:05 -0700

    Add read/user input and list of available fonts

--- font-production/generatestylesheet.sh
+++ font-production/generatestylesheet.sh
@@ -1,21 +1,28 @@
 # Generate Stylesheets
 # Created by Kyle Wayne Benson
 
-# Notes: edit the fontname variable on line 11 to be your folder name. Make sure there are no spaces in your folder name.
-# This script needs to be in a directory named docs next to the folder filled with fonts that you're referencing.
-# Edit the array on line 12 so that your font style variables align with the weight classes you want.
-
-fontname=yourFolderName
-> "../$fontname/$fontname.css"
-echo "/* CSS for ${fontname} generated $(date +%F) */" >> "$fontname/$fontname.css"
-echo "/* CSS Generator made by Kyle Wayne Benson */" >> "$fontname/$fontname.css"
-ARRAY=( "300:Light"
-        "400:Regular"
-        "700:Bold"
-        "800:Extrabold"
-        "900:Black"
-         )
+# Notes: You will be prompted for a font family name. Make sure there are no spaces in your folder name.
+# This script needs to be in a folder next to the folder filled with fonts that you're referencing.
 cd ..
+echo ">> What is the name of the folder/font family? Options are:"
+echo 
+for folder in *; do 
+	echo " \t $folder"
+done
+read fontname
+echo "/* CSS for ${fontname} generated $(date +%F) */" > $fontname/$fontname.css
+echo "/* CSS Generator made by Kyle Wayne Benson */" >> $fontname/$fontname.css
+ARRAY=( "100:Thin"
+        "200:Extralight"
+        "200:ExtraLight"
+        "300:Light"
+        "400:Regular"
+        "500:Medium"
+        "600:SemiBold"
+        "600:Semibold"
+        "700:Bold"
+        "800:ExtraBold"
+        "900:Black" )
 
 for FILE in $fontname/*.otf
 	do FONT=${FILE##*/}
@@ -23,7 +30,7 @@ for FILE in $fontname/*.otf
 	DIR=${FILE%/*}
 	STYLE=${FONTNAME##*-}
 	FAMILY=${FONT%-*}
-	family=`echo "$fontname" | tr '[:upper:]' '[:lower:]'`
+	family=`echo "$fontname" | tr '[:upper:]' '[:lower:]'` 
 
 	for WEIGHT in "${ARRAY[@]}" ; do
 	    KEY="${WEIGHT%%:*}"
